48800c15f5d56cfbfc5474df2617e618b78dcf31
therealzteve <htczteve@gmail.com>
2014-05-27 18:16:55 +0200

    Added new Actions

---
+++ Monopoly/src/actions/BuyAction.java
@@ -0,0 +1,13 @@
+package actions;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class BuyAction extends GameBaseAction {
+
+	@Override
+	void performAction(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
