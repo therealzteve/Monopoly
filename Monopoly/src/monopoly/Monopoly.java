bf87b527a1d2ae6d48a5437d6b9489f6ab3c9e8f
Simsal <simon_brueckl9@hotmail.com>
2014-06-16 16:03:11 +0200

    updated monopoly aktionsfelder

--- Monopoly/src/monopoly/Monopoly.java
+++ Monopoly/src/monopoly/Monopoly.java
@@ -17,12 +17,12 @@ public class Monopoly {
 	public void erstelleStrassen(){
 		 Feld Los = new Feld(0, "Los", "los");
 		 Feld Bad = new Street(1, "Badstrasse", 0, 60,2,10,30,90,160,250);
-		 Feld Ereignisfeld1 =new Aktionsfeld(2, "Ereignisfeld", "aktion");
+		 Feld Ereignisfeld1 =new Aktionsfeld(2, "Ereignisfeld", "aktion", this);
 		 Feld Turm = new Street(3, "Turmstrasse", 0, 60,4,20,60,180,320,450);
-		 Feld Einkommenssteuer = new Aktionsfeld(4, "Einkommenssteuer", "aktion");
+		 Feld Einkommenssteuer = new Aktionsfeld(4, "Einkommenssteuer", "aktion", this);
 		 Feld Suedbahnhof = new Street (5, "Suedbahnhof", "bahnhof");
 		 Feld Chausse = new Street(6,"Chaussestrasse", 1,100,6,30,90,270,400,550);
-		 Feld Ereignisfeld2 = new Aktionsfeld(7, "Ereignisfeld", "aktion");
+		 Feld Ereignisfeld2 = new Aktionsfeld(7, "Ereignisfeld", "aktion", this);
 		 Feld Elisen =new Street (8, "Elisenstrasse", 1,100,6,30,90,270,400,550);
 		 Feld Post = new Street(9, "Poststraße", 1,120,8,40,100,300,400,550);
 		 Feld Gefaengnis = new Feld (10, "Gefaengnis", "gefaengnis");
@@ -32,12 +32,12 @@ public class Monopoly {
 		 Feld Neue = new Street (14, "Neue Strasse", 2, 160,12,60,180,500,700,900);
 		 Feld Westbahnhof = new Street (15, "Westbahnhof", "bahnhof");
 		 Feld Muenchener = new Street(16, "Muenchener Strasse", 3, 180,14,70,200,550,750,950);
-		 Feld Gemeinschaftsfeld1 = new Aktionsfeld (17, "Gemeinschaftsfeld", "aktion");
+		 Feld Gemeinschaftsfeld1 = new Aktionsfeld (17, "Gemeinschaftsfeld", "aktion", this);
 		 Feld Wiener = new Street (18, "Wiener Strasse", 3,180,14,70,200,550,750,950);
 		 Feld Berliner = new Street (19, "Berliner Strasse", 3,200,16,80,220,600,800,1000);
 		 Feld FreiParken = new Feld (20, "Frei Parken", "freiParken");
 		 Feld Theater = new Street (21, "Theaterstrasse", 4,220,18,90,250,700,875,1050);
-		 Feld Ereignisfeld3 = new Aktionsfeld (22, "Ereignisfeld", "aktion");
+		 Feld Ereignisfeld3 = new Aktionsfeld (22, "Ereignisfeld", "aktion", this);
 		 Feld Museum = new Street (23, "Museumsstrasse", 4,220,18,90,250,700,875,1050);
 		 Feld Opernplatz = new Street (24, "Opernplatz", 4,240,20,100,300,750,925,1100);
 		 Feld Nordbahnhof = new Street (25, "Nordbahnhof", "bahnhof");
@@ -48,10 +48,10 @@ public class Monopoly {
 		 Feld GeheInsGefaengnis = new Feld (30, "Gehe ins Gefaengnis!", "geheGefaengnis");
 		 Feld Rathausplatz = new Street (31, "Rathausplatz", 6,300,26,130,390,900,1100,1275);
 		 Feld Haupt = new Street (32, "Hauptstrasse", 6,300,26,130,390,900,1100,1275);
-		 Feld Gemeinschaftsfeld2 = new Aktionsfeld (33, "Gemeinschaftsfeld", "aktion");
+		 Feld Gemeinschaftsfeld2 = new Aktionsfeld (33, "Gemeinschaftsfeld", "aktion", this);
 		 Feld Bahnhof = new Street (34, "Bahnhofsstrasse", 6,320,28,150,450,1100,1200,1400);
 		 Feld Hauptbahnhof = new Street (35, "Hauptbahnhof", "bahnhof");
-		 Feld Ereignisfeld4 = new Aktionsfeld(36, "Ereignisfeld", "aktion");
+		 Feld Ereignisfeld4 = new Aktionsfeld(36, "Ereignisfeld", "aktion", this);
 		 Feld Park = new Street (37, "Parkstrasse", 7,350,35,175,500,1100,1300,1500);
 		 Feld Zusatzsteuer = new Feld (38, "Zusatzsteuer", "aktion");
 		 Feld Schlossallee = new Street (39, "Schlossallee", 7,400,50,200,600,1400,1700,2000);
